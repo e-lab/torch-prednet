4b466a8307cb8f17a3c3a135a946114463698174
spk <spkim921@gmail.com>
2016-09-30 12:59:07 -0400

    update comments

--- convLSTM.lua
+++ convLSTM.lua
@@ -1,4 +1,5 @@
 -- First written by Sangpil Kim
+-- Notation is from https://github.com/oxford-cs-ml-2015/practical6/blob/master/LSTM.lua
 -- ConvLSTM with nngraph
 -- August 2016
 
@@ -18,10 +19,10 @@ function convLSTM(inDim, outDim, opt)
   -- Input  is 1+ 2*#Layer
   -- Output is 1+ 2*#Layer
   local inputs = {}
-  table.insert(inputs, nn.Identity()()) -- x
+  table.insert(inputs, nn.Identity()()) -- X
   for L = 1,n do
-    table.insert(inputs, nn.Identity()()) -- prevC[L]
-    table.insert(inputs, nn.Identity()()) -- prevH[L]
+    table.insert(inputs, nn.Identity()()) -- Cell
+    table.insert(inputs, nn.Identity()()) -- Hidden state
   end
 
   local x
@@ -30,58 +31,57 @@ function convLSTM(inDim, outDim, opt)
      -- Container for previous C and H
     local prevH = inputs[L*2+1]
     local prevC = inputs[L*2]
-    -- Setup input
+    -- Get input
     if L == 1 then
-      x = inputs[1] --This form is from neuraltalk2
+      x = inputs[1]
     else
-    -- Prev hidden output
+    -- Get x from bottom layer as input
       x = outputs[(L-1)*2]
-      if dropout > 0 then x = nn.Dropout(dropout)(x) end -- apply dropout, if any
     end
-    -- In put convolution
+    --Convolutions
     local i2Ig, i2Fg, i2Og, i2It
     if L == 1 then
-       i2Ig = sc(inDim, outDim, kw, kh, stw, sth, paw, pah)(x):annotate{name='i2Ig_'..L}
-       i2Fg = sc(inDim, outDim, kw, kh, stw, sth, paw, pah)(x):annotate{name='i2Fg_'..L}
-       i2Og = sc(inDim, outDim, kw, kh, stw, sth, paw, pah)(x):annotate{name='i2Og_'..L}
-       i2It = sc(inDim, outDim, kw, kh, stw, sth, paw, pah)(x):annotate{name='i2It_'..L}
+       i2Ig = sc(inDim, outDim, kw, kh, stw, sth, paw, pah)(x)
+       i2Fg = sc(inDim, outDim, kw, kh, stw, sth, paw, pah)(x)
+       i2Og = sc(inDim, outDim, kw, kh, stw, sth, paw, pah)(x)
+       i2It = sc(inDim, outDim, kw, kh, stw, sth, paw, pah)(x)
     else
-       i2Ig = sc(outDim, outDim, kw, kh, stw, sth, paw, pah)(x):annotate{name='i2Ig_'..L}
-       i2Fg = sc(outDim, outDim, kw, kh, stw, sth, paw, pah)(x):annotate{name='i2Fg_'..L}
-       i2Og = sc(outDim, outDim, kw, kh, stw, sth, paw, pah)(x):annotate{name='i2Og_'..L}
-       i2It = sc(outDim, outDim, kw, kh, stw, sth, paw, pah)(x):annotate{name='i2It_'..L}
+       i2Ig = sc(outDim, outDim, kw, kh, stw, sth, paw, pah)(x)
+       i2Fg = sc(outDim, outDim, kw, kh, stw, sth, paw, pah)(x)
+       i2Og = sc(outDim, outDim, kw, kh, stw, sth, paw, pah)(x)
+       i2It = sc(outDim, outDim, kw, kh, stw, sth, paw, pah)(x)
     end
 
-    local h2Ig = scNB(outDim, outDim, kw, kh, stw, sth, paw, pah)(prevH):annotate{name='h2Ig_'..L}
-    local h2Fg = scNB(outDim, outDim, kw, kh, stw, sth, paw, pah)(prevH):annotate{name='h2Fg_'..L}
-    local h2Og = scNB(outDim, outDim, kw, kh, stw, sth, paw, pah)(prevC):annotate{name='h2Og_'..L}
-    local h2It = scNB(outDim, outDim, kw, kh, stw, sth, paw, pah)(prevC):annotate{name='h2It_'..L}
+    local h2Ig = scNB(outDim, outDim, kw, kh, stw, sth, paw, pah)(prevH)
+    local h2Fg = scNB(outDim, outDim, kw, kh, stw, sth, paw, pah)(prevH)
+    local h2Og = scNB(outDim, outDim, kw, kh, stw, sth, paw, pah)(prevC)
+    local h2It = scNB(outDim, outDim, kw, kh, stw, sth, paw, pah)(prevC)
 
     local ig = nn.CAddTable()({i2Ig, h2Ig})
     local fg = nn.CAddTable()({i2Fg, h2Fg})
     local og = nn.CAddTable()({i2Og, h2Og})
     local it = nn.CAddTable()({i2It, h2It})
 
-    -- Gates calculation
+    -- Gates
     local inGate = sg()(ig)
     local fgGate = sg()(fg)
     local ouGate = sg()(og)
     local inTanh = nn.Tanh()(it)
-    -- perform the LSTM update
+    -- Calculate Cell state
     local nextC = nn.CAddTable()({
         nn.CMulTable()({fgGate, prevC}),
         nn.CMulTable()({inGate, inTanh})
       })
-    -- gated cells form the output
+    -- Calculate output
     local out = nn.CMulTable()({ouGate, nn.Tanh()(nextC)})
 
     table.insert(outputs, nextC)
-   --Apply dropout
-   if dropout > 0 then out = nn.Dropout(dropout)(nextH):annotate{name='drop_final'} end
+   -- Dropout if neccessary
+   if dropout > 0 then out = nn.Dropout(dropout)(nextH) end
     table.insert(outputs, out)
   end
 
-  -- Get last output
+  -- Extract output
   local lastH = outputs[#outputs]
 
   return nn.gModule(inputs, outputs)
